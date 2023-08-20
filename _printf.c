6376fed3fbf856c802defef3361db1f1e9f6292d
kofilolx <kofyamando@gmail.com>
2023-08-20 10:19:18 -0400

    under-construction

--- _printf.c
+++ _printf.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * _printf - Function that generates output in
+ * accordance with specified format.
+ * @format: datatypes (int, char, double)
+ *
+ * Return: Output to standard console
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+	/* should return and int value */
+	return (int);
+}
+
--- main.h
+++ main.h
@@ -9,6 +9,25 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/* function prototype of list of specifier */
+int print_char(va_list c);
+int print_string(va_list s);
+
+/**
+ * struct function_syntax - list of datatypes and specifiers
+ * @specifier: "%d", "%s", "%i"
+ * @f: function pointer point va_list
+ *
+ * Return: nothing
+ */
+
+typedef struct function_syntax
+{
+	char *specifier;
+	int (*f)(va_list);
+
+} f_syntax;
+
 
 
 #endif
