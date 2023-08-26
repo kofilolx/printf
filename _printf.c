4d4d92a8c64d8082c64ac4304687788f401c4522
kofilolx <kofyamando@gmail.com>
2023-08-26 13:56:11 -0400

    replaced file

---
+++ _printf.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * _printf - print f function
+ * @format: character string will be printed
+ * Return: number of digits printed
+ */
+int _printf(const char *format, ...)
+{
+	int j, itr, nxt_char = 0;
+	va_list argList;
+
+	f_syntax sp_f[] = {
+		{'c', prt_char},
+		{'s', prt_string},
+		{'%', prt_mod},
+		{'i', prt_int},
+		{'d', prt_int},
+		{'r', rev_string},
+		{'x', prt_hex},
+		{'X', prt_Hex},
+		{'o', prt_octal},
+		{'u', prt_unsigned},
+		{'b', prt_bin},
+		{'p', prt_add},
+		{'S', prt_ex_str},
+		{NULL, NULL}
+	};
+	/* initialization of arg list */
+	va_start(argList, format);
+	if ((!format) || (format[0] == '%' && !format[1]))
+		return (-1);
+	while (format[itr])
+	{
+		if (format[itr] == '%')
+		{
+			if (format[itr + 1] == '\0')
+				return (-1);
+			if (format[itr + 1] == '+' || format[itr + 1] == ' ')
+			{
+				va_list dst;
+
+				va_copy(dst, argList);
+				nxt_char += op_space(format[itr + 1], va_arg(dst, int));
+				itr++;
+			}
+			else if (format[itr + 1] == '#')
+			{
+				nxt_char += _hash(format[itr + 2], &i);
+			}
+
+			j = 0;
+			while (j < 13)
+			{
+				if (format[itr + 1] == sp_f[j].specifier)
+				{
+					nxt_char += sp_f[j].f(argList);
+					itr += 2;
+					break;
+				}
+				j++;
+			}
+		}
+		else
+		{
+			_putchar(format[itr]);
+			nxt_char++;
+			itr++;
+		}
+	}
+	/* cleans up the argument list */
+	va_end(argList);
+	return (nxt_char);
+}
---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef_MAIN_H
+#define_MAIN_H
+
+/* Inclusion of External Libraries **/
+include <stdarg.h>
+include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/* Prototypes function container */
+int _putchar (int n);
+int _printf (const char *format, ...);
+int op_space(int c, int n);
+int _hash (char c, int *i);
+
+/**
+* struct prog_structure - Struct format
+* @specifier: Formats char, int, decimal, string
+* @f: Function pointer
+* */
+struct prog_structure {
+char *specifier;
+int (*f)(va_list);
+} f_syntax;
+
+void prt_Hexadecimal(unsigned int num, int *p);
+int prt_ex_str(va_list argList);
+int prt_hex_ptr(va_list argList);
+int prt_address(va_list argList);
+int prt_binary(va_list argList);
+int prt_unsigned(va_list argList);
+int prt_octal(va_list argList);
+int prt_hex(va_list argList);
+int prt_Hex(va_list argList);
+int rev_string(va_list argList);
+int len_int(int argList);
+int prt_char(va_list argList);
+int prt_mod(va_list argList);
+int prt_int(va_list argList);
+int prt_string(va_list argList);
