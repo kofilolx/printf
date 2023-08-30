dd6e55057155c1717784f7291364fb9d2470958a
kofilolx <kofyamando@gmail.com>
2023-08-29 23:08:21 -0400

    i'm just tired i will surely sleep to night

--- prt_char.c
+++ prt_char.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * prt_char - print input as char
- * @argList: pointer
+ * prt_char - writes to the std as char
+ * @c: char to be printed
  * Return: char
  */
-int prt_char(va_list argList)
+int prt_char(va_list c)
 {
 	_putchar(va_arg(argList, int));
+	return (1);
+}
+
+/**
+ * prt_percentage - percent symbol %
+ *
+ * return: 1 success
+ */
+int prt_percentage(void)
+{
+	_putchar('%');
 	return (1);
 }
